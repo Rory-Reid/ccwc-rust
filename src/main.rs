b69b8c8045c22a2d828ed5d839e83f74d5928721
Rory Reid <rory.roberts@outlook.com>
2024-12-29 23:07:52 +0000

    Complete step one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::File;
+use std::io::{Seek, SeekFrom};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command()]
+struct Args {
+  #[arg()]
+  file: String,
+
+  #[arg(short = 'c')]
+  count_bytes: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args = Args::parse();
+
+  match args.count_bytes {
+    true => {}
+    false => panic!("Only the -c flag is implemented currently!")
+  }
+
+  let mut file = match File::open(&args.file) {
+    Ok(f) => f,
+    Err(_) => panic!("File not found") // Todo
+  };
+
+  // This is a bit of a shortcut for satisfying the -c flag only currently
+  let size = match file.seek(SeekFrom::End(0)) {
+    Ok(size) => size,
+    Err(_) => panic!("Cannot seek") // Todo
+  };
+
+  println!("{} {}", format!("{: >8}", size), args.file);
 }
