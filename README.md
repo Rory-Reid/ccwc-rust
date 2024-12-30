70979b4a067d29b9f01df2ac3bce8654566c3ee0
Rory Reid <rory.roberts@outlook.com>
2024-12-30 23:21:41 +0000

    Complete step two

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ccwc in Rust
 
 This repository [implements a wc-inspired application as part of a coding challenge](https://codingchallenges.fyi/challenges/challenge-wc) in Rust.
+
+## Decisions
+
+### Buffer
+
+In an interesting exercise of self-gaslighting, I had convinced myself that the coding challenge had some late-game stretch goal suggesting that you try to make it work with a 100gb file. I'm not made of money and my RAM is too small for that, so I decided that manually buffering it into memory a chunk at a time would be better than relying on some handy "read file as string" method provided to me (and yes, I understand that can result in a slower process due to increased IO calls). On re-reading the challenge, I can't find any mention of it and cannot imagine where I pulled this requirement from.
+
+Luckily, this is a coding challenge I'm doing exclusively in the evening to learn rust, and I'm not being paid for it, so who cares.
+
+Also I picked a maximum 100mb buffer size based on an arbitrary "you won't notice it missing from your ram, but that's probably enough for now" without overthinking it too much.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Seek, SeekFrom};
+use std::io::{Read, Seek, SeekFrom};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -10,26 +10,66 @@ struct Args {
 
   #[arg(short = 'c')]
   count_bytes: bool,
+
+  #[arg(short = 'l')]
+  count_lines: bool,
 }
 
 fn main() {
   let args = Args::parse();
 
-  match args.count_bytes {
+  match args.count_bytes || args.count_lines {
     true => {}
-    false => panic!("Only the -c flag is implemented currently!")
+    false => panic!("Current scope only supports -l and/or -c flags. At least one must be present.")
   }
 
-  let mut file = match File::open(&args.file) {
-    Ok(f) => f,
-    Err(_) => panic!("File not found") // Todo
+  let mut file = File::open(&args.file).unwrap(); // Todo - handle error and don't just unwrap
+  file.seek(SeekFrom::Start(0)).unwrap();
+
+  let mut bytes_read : usize;
+  let mut total_bytes : usize = 0;
+  let mut lines : i64 = 0;
+
+  // This essentially creates a fixed-length vector on the heap pre-allocated so that we can use it
+  // as a buffer. I didn't use an array because it overflows the stack, and I had issues with that
+  // when trying to allocate a boxed array too. The vector is pre-initialised to "full" capacity
+  // because the file.read() function will fill a buffer to current capacity, i.e. a vec of capacity
+  // 100 but current size 0 will result in reading 0 bytes. See https://stackoverflow.com/questions/41710952/allocate-array-onto-heap-with-size-known-at-runtime
+  const MAX_BUF_SIZE: usize = 104_857_600; // 100mb
+  let file_size = file.seek(SeekFrom::End(0)).unwrap() as usize;
+  file.seek(SeekFrom::Start(0)).unwrap();
+  let buf_size = if file_size > MAX_BUF_SIZE {MAX_BUF_SIZE} else {file_size};
+  let mut buffer = vec![0; file_size];
+  loop {
+    bytes_read = file.read(&mut buffer).unwrap(); // Todo - handle error and don't just unwrap
+    if bytes_read == 0 {
+      break;
+    }
+
+    for index in 0..bytes_read {
+      if buffer[index] == b'\n' {
+        lines += 1;
+      }
+    }
+
+    total_bytes += bytes_read;
+    if bytes_read < buf_size {
+      // end of file
+      break;
+    }
+
+    _ = file.seek(SeekFrom::Start(total_bytes as u64)).unwrap() // Todo - error handling
+  }
+
+  let bytes_output = match args.count_bytes {
+    true => format!("{: >8}", total_bytes),
+    false => String::new()
   };
 
-  // This is a bit of a shortcut for satisfying the -c flag only currently
-  let size = match file.seek(SeekFrom::End(0)) {
-    Ok(size) => size,
-    Err(_) => panic!("Cannot seek") // Todo
+  let lines_output = match args.count_lines {
+    true => format!("{: >8}", lines),
+    false => String::new()
   };
 
-  println!("{} {}", format!("{: >8}", size), args.file);
+  println!("{}{} {}", lines_output, bytes_output, args.file);
 }
